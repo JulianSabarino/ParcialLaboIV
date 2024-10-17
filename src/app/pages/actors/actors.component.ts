dd929edc72ecfba5bdddc7e2ab95421e4a9a4fd7
Julian Sabarino <julian.sabarino@gmail.com>
2024-10-17 10:48:43 -0300

    sprint 4

--- src/app/pages/actors/actors.component.ts
+++ src/app/pages/actors/actors.component.ts
@@ -1,18 +1,40 @@
-import { Component, inject } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { Actor } from '../../models/actor.model';
+import { ActorsService } from '../../services/actors.service';
+import { CommonModule } from '@angular/common';
+import { FilmsService } from '../../services/films.service';
+import { Film } from '../../models/film.model';
 
 @Component({
   selector: 'app-actors',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './actors.component.html',
   styleUrl: './actors.component.scss'
 })
-export class ActorsComponent {
-  rootService = inject(Router);
+export class ActorsComponent implements OnInit {
 
-  moveTo()
-  {
-    this.rootService.navigate(["./abm"]);
+  actorList!: Actor[];
+  actorsService = inject(ActorsService);
+  filmService = inject(FilmsService);
+  filmList!: Film[];
+
+  selectedActor!: Actor;
+  selectedFilmList!: Film[];
+
+  async ngOnInit() {
+    this.actorList = await this.actorsService.getActors();
+    console.log(this.actorList);
+    this.filmList = await this.filmService.getFilms();
+    console.log(this.filmList);
   }
+
+  selectActor(actor : Actor)
+  {
+    this.selectedActor = actor;
+    console.log(this.selectedActor);
+    this.selectedFilmList = this.filmList.filter(film => film.protagonist.document === this.selectedActor.document);
+
+  }
+
 }
